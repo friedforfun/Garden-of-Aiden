68e046021c9401a5249ff251df75e65c20d74c25
Sam Fay-Hunt <samfayhunt@gmail.com>
2021-07-06 01:45:07 +0100

    tensorflow starter stuff

---
+++ Assets/Scripts/python/DCGAN/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name="DCGAN-Garden-of-Alden", 
+    version=0.01,
+    url='https://github.com/friedforfun/Garden-of-Alden',
+    author='',
+    author_email='',
+    packages=find_packages()
+)
